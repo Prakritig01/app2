f46372a0400a71591e766bf9ca5eb27a5c8687f6
Prakritig01 <prakritigupta1112002@gmail.com>
2024-12-02 15:06:34 +0530

    use of effect

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,22 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import "./Counter.css";
+import backend from "../../Service/backend";
 
 const Counter = ({ name }) => {
   const [count, setCount] = useState(0);
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
-    console.log("Counter created!" ,name);
-    return () => console.log("Counter Destroyed!",name);
+    backend.connect(name).then(prevCount => setCount(prevCount));
+    return () => backend.disconnect(name);
   }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    backend.updateMap(count);
+  }, [count]);
 
   const increment = () => setCount((prev) => prev + 1);
   const reset = () => setCount(0);
+  const ping = () => {backend.ping("hii",name);}
   return (
     <div>
       <div className="counter">count :{count}</div>
       <div className="name">{name}</div>
       <button onClick={increment}>Increment</button>
       <button onClick={reset}>Reset</button>
+      <button onClick={ping}>Ping Me!</button>
       <div className="message">{count > 10 ? "Threshold reached !!!" : ""}</div>
     </div>
   );
